5b5413c07d148e9d36a7a2569882701ad1873164
Nika Abasi <sabbasi60@gmail.com>
2023-04-01 00:57:45 +0430

    server deploy config

---
+++ backend/database/DB.js
@@ -0,0 +1,3 @@
+module.exports={
+   DB:'mongodb://machinya_machinya:Nika!805210@localhost:27017/machinya_db'
+}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const { default: mongoose } = require('mongoose');
+const { prototype } = require('nodemailer/lib/xoauth2');
 
 dotenv.config();
 
@@ -11,17 +12,19 @@ const port = +process.env.PORT;
 //   res.sendFile(__dirname+'/frontend/public/index.html'); // change the path to your index.html
 // });
 
-(async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log('mongodb connected.');
-})();
+// (async () => {
+//   await mongoose.connect(process.env.MONGO_URI);
+//   console.log('mongodb connected.');
+// })();
 
 
 mongoose.connect('mongodb://machinya_machinya:Nika!805210@localhost:27017/machinya_db', {
   useNewUrlParser: true,
   authSource: 'admin'
+  .then(()=> console.log('connected to mongodb'))
+.catch((err)=> console.log('could not connect to mongodb'))
 });
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
